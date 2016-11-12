1f89cd7727bc4442f94105f0144f81ff0abd031d
Michael Facul <Michael Facul@Michael-PC>
2016-11-12 18:16:18 -0200

    Ajuste de erros

--- src/main/java/br/sp/senac/programeros/dao/ClienteDAO.java
+++ src/main/java/br/sp/senac/programeros/dao/ClienteDAO.java
@@ -39,8 +39,7 @@ public class ClienteDAO implements br.sp.senac.programeros.interfaces.ClienteInt
             p.setString(7, String.valueOf(cliente.getSexo()));
             p.setString(8, cliente.getTelefone());
             p.setString(9, cliente.getCelular());
-            p.setDate(10, new java.sql.Date(System.currentTimeMillis()));
-            
+            p.setDate(10, new java.sql.Date(System.currentTimeMillis()));            
             p.setString(11, "S");
 
             p.execute();
@@ -57,8 +56,8 @@ public class ClienteDAO implements br.sp.senac.programeros.interfaces.ClienteInt
         try {
             String sql = "UPDATE clientes "
                     + " SET nome = ?, endereco = ?, bairro = ?, cidade = ?, "
-                    + " estado = ?, cep = ?, sexo = ?, telefone = ?, celular = ?,"
-                    + " convenio_codigo = ?"
+                    + " estado = ?, cep = ?, telefone = ?, celular = ?, convenio_codigo = ?"
+                   
                     + " WHERE codigo = ?";
 
             PreparedStatement p;
@@ -69,11 +68,11 @@ public class ClienteDAO implements br.sp.senac.programeros.interfaces.ClienteInt
             p.setString(4, cliente.getCidade());
             p.setString(5, cliente.getEstado());
             p.setString(6, cliente.getCep());
-            p.setString(7, String.valueOf(cliente.getSexo()));
-            p.setString(8, cliente.getTelefone());
-            p.setString(9, cliente.getCelular());
-            p.setInt(10, cliente.getConvenio());            
-            p.setInt(13, cliente.getCodigo());
+            //p.setString(7, String.valueOf(cliente.getSexo()));
+            p.setString(7, cliente.getTelefone());
+            p.setString(8, cliente.getCelular());
+            p.setInt(9, cliente.getConvenio());            
+            p.setInt(10, cliente.getCodigo());
 
             p.execute();
 
