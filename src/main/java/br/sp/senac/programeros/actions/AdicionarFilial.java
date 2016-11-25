6b8234dcdd1687e1e3729a1887713f711b2478cc
Pedro Blasembauer <losvigaristas157@gmail.com>
2016-11-25 19:25:00 -0200

    ATTS SERVLETS FILIAL

---
+++ src/main/java/br/sp/senac/programeros/actions/AdicionarFilial.java
@@ -0,0 +1,71 @@
+package br.sp.senac.programeros.actions;
+
+import br.sp.senac.programeros.connection.ConexaoBD;
+import br.sp.senac.programeros.dao.FilialDAO;
+import br.sp.senac.programeros.model.Filiais;
+import com.mysql.jdbc.Connection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdicionarFilial extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String nome = request.getParameter("nome");
+        String endereco = request.getParameter("endereco");
+        String bairro = request.getParameter("bairro");
+        String cidade = request.getParameter("cidade");
+        String estado = request.getParameter("estado");
+        String cep = request.getParameter("cep");
+        String telefone = request.getParameter("telefone");
+        String fax = request.getParameter("fax");
+        String responsavel = request.getParameter("responsavel");
+        String ativo = "S";
+        
+        Filiais novaFilial = new Filiais();
+        novaFilial.setNome(nome);
+        novaFilial.setEndereco(endereco);
+        novaFilial.setBairro(bairro);
+        novaFilial.setCidade(cidade);
+        novaFilial.setEstado(estado);
+        novaFilial.setCep(cep);
+        novaFilial.setTelefone(telefone);
+        novaFilial.setFax(fax);
+        novaFilial.setResponsavel(responsavel);
+        novaFilial.setAtivo(ativo);
+
+        ConexaoBD conn = new ConexaoBD();
+        Connection conexao = conn.obterConexao();
+
+        FilialDAO dao = new FilialDAO(conexao);
+        dao.inserir(novaFilial);
+
+        conn.fecharConexao();
+
+        response.sendRedirect("/Farmacia/Filiais/filial.jsp");
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ src/main/java/br/sp/senac/programeros/actions/ExcluirFilial.java
@@ -0,0 +1,49 @@
+package br.sp.senac.programeros.actions;
+
+import br.sp.senac.programeros.connection.ConexaoBD;
+import br.sp.senac.programeros.dao.FilialDAO;
+import com.mysql.jdbc.Connection;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExcluirFilial extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String codigo = request.getParameter("id");
+        
+        int id = Integer.parseInt(codigo);
+        
+        ConexaoBD conn = new ConexaoBD();
+        Connection conexao = conn.obterConexao();
+
+        FilialDAO dao = new FilialDAO(conexao);
+        dao.Remove(id);
+
+        conn.fecharConexao();
+        
+        response.sendRedirect("/Farmacia/Filiais/filial.jsp");    
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
